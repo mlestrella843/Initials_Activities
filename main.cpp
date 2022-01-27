dfade2ef929fe40958d09260a748a1fde2ea0d04
maria0843 <>
2022-01-27 22:04:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string name;
+int age;
+
+std::cout <<"What is your name?\n";
+cin>>name;
+
+std::cout <<"What is your age?\n";
+cin>>age;
+
+std::cout <<"Hello\t" <<name<< "\t" <<age;
+
+
+return 0;
+
+
+}
+
